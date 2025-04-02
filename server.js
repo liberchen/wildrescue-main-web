37afe07b9ce230f150295aeb6ccd0e9a1db1bd4c
liberchen <liberchen@gmail.com>
2025-04-02 16:19:25 +0800

    Add privacy-policy.html and term-of-service.html

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Debug middleware：紀錄每個請求
+app.use((req, res, next) => {
+    console.log(`[DEBUG] ${req.method} ${req.url}`);
+    next();
+});
+
 // 設定 public 資料夾為靜態資源目錄
 app.use(express.static(path.join(__dirname, 'public')));
 
